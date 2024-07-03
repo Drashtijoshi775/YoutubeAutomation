05eb92fb766d441319027410579698a5379aaabd
Dhruval Bhatt <drashtijoshi0@gmail.com>
2024-07-03 17:25:28 +0530

    ME_QA_XYOUTUBE_SEARCH_MODULE_L1

--- src/test/java/demo/TestCases.java
+++ src/test/java/demo/TestCases.java
@@ -1,6 +1,7 @@
 package demo;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -18,6 +19,8 @@ import org.testng.asserts.SoftAssert;
 
 import java.time.Duration;
 import java.util.logging.Level;
+
+import javax.swing.text.Utilities;
 
 import demo.utils.ExcelDataProvider;
 // import io.github.bonigarcia.wdm.WebDriverManager;
@@ -57,6 +60,167 @@ public class TestCases extends ExcelDataProvider{ // Lets us read the data
 
                 driver.manage().window().maximize();
         }
+
+         @BeforeMethod
+    public void goToYT() throws InterruptedException{
+        Wrappers.goToUrlAndWait(driver, "https://youtube.com");
+        Thread.sleep((new java.util.Random().nextInt(3) + 2) * 1000);
+    }
+
+    @Test(enabled = true)
+    public void testCase01() throws InterruptedException {
+        System.out.println("Running Test Case 01");
+
+        // Asserting correct URL
+        SoftAssert sa = new SoftAssert();
+        sa.assertTrue(driver.getCurrentUrl().contains("youtube.com"), "URL does not contain 'youtube.com'");
+
+        // Clicking on "About"
+        Wrappers.findElementAndClick(driver, By.xpath("//div[@id='guide-links-primary']//a[text()='About']"));
+
+        // Printing the message on the screen
+        String message = Wrappers.findElementAndPrint(driver, By.tagName("h1"), 0);
+        System.out.println("Message on the screen: " + message);
+
+        sa.assertAll();
+    }
+
+    @Test(enabled = true)
+    public void testCase02() throws InterruptedException {
+        System.out.println("Running Test Case 02");
+
+        // Go to Films tab
+        SoftAssert softAssert = new SoftAssert();
+          
+          // Navigate to the "Films" tab
+         WebElement movies = driver.findElement(By.xpath("//yt-formatted-string[text()='Movies']"));
+         movies.click();
+          
+          // Wait for the page to load completely
+          Thread.sleep(2000);
+          
+          // Scroll to the extreme right in the "Top Selling" section
+          WebElement topSellingSection = driver.findElement(By.xpath("(//ytd-button-renderer[@class='style-scope yt-horizontal-list-renderer arrow'])[2]"));
+          // JavascriptExecutor js = (JavascriptExecutor) driver;
+          // js.executeScript("arguments[0].scrollLeft = arguments[0].scrollWidth", topSellingSection);
+          
+          while (topSellingSection.isDisplayed()) {
+            
+              topSellingSection.click();
+              Thread.sleep(1000);
+          }
+
+               
+         // Apply Soft Assert on maturity rating "A"
+    boolean isRatedA = false;
+    try {
+        isRatedA = Wrappers.findElementAndPrint(driver, By.xpath("//div[@class='badge badge-style-type-simple style-scope ytd-badge-supported-renderer style-scope ytd-badge-supported-renderer']//p[text()='A']"), 0)
+                        .equalsIgnoreCase("A");
+    } catch (NoSuchElementException e) {
+        // Handle if element is not found
+    }
+
+    SoftAssert sa = new SoftAssert();
+    sa.assertTrue(isRatedA, "Movie is not rated 'A' for Mature");
+
+    // Apply Soft Assert on genre "Comedy" or "Animation"
+    boolean isComedyOrAnimation = false;
+    try {
+        WebElement comedyElement = driver.findElement(By.xpath("//*[@id=\"items\"]/ytd-grid-movie-renderer[16]/a/span"));
+        WebElement animationElement = driver.findElement(By.xpath("//*[@id=\"items\"]/ytd-grid-movie-renderer[16]/a/span"));
+        if (comedyElement.isDisplayed() || animationElement.isDisplayed()) {
+            isComedyOrAnimation = true;
+        }
+    } catch (NoSuchElementException e) {
+        // Handle if element is not found
+    }
+    sa.assertTrue(isComedyOrAnimation, "Movie is neither Comedy nor Animation");
+
+    sa.assertAll(); // This will throw AssertionError if any soft assertion fails
+}
+
+        //   WebElement matureMovie = driver.findElement(By.xpath("//*[@id=\"items\"]/ytd-grid-movie-renderer[6]/ytd-badge-supported-renderer/div[2]/p"));
+
+        //   String ratingMovieText = matureMovie.getText();
+        //   softAssert.assertEquals(ratingMovieText,"A");
+
+        //   System.out.println("verified the rating-"+ ratingMovieText);
+
+          
+        //   // Verify if the movie genre is either "Comedy" or "Animation"
+        //   WebElement movieGenre = driver.findElement(By.xpath("(//span[contains(text(),'Comedy')])[3]"));
+
+        //   String GenreMovieText = movieGenre.getText();
+        //   softAssert.assertTrue(GenreMovieText.contains("Comedy"),"Unverified Genre");
+
+        //   System.out.println("verified the GenreMovie-"+ GenreMovieText);
+
+    
+        //   softAssert.assertAll();
+        //   System.out.println("End Test case: testCase02");
+        // }
+
+    @Test(enabled = true)
+    public void testCase03() throws InterruptedException{
+        System.out.println("Running Test Case 03");
+        Wrappers.findElementAndClick(driver, By.xpath("//a[@title='Music']"));
+        Thread.sleep((new java.util.Random().nextInt(3) + 2) * 1000);
+        Wrappers.clickTillUnclickable(driver, By.xpath("(//span[contains(.,'Biggest Hits')]//ancestor::div[5]//button[@class='yt-spec-button-shape-next yt-spec-button-shape-next--text yt-spec-button-shape-next--mono yt-spec-button-shape-next--size-m yt-spec-button-shape-next--icon-only-default'])[2]/.."), 5);
+        Thread.sleep((new java.util.Random().nextInt(3) + 2) * 1000);
+        By locator_TrackCount = By.xpath("//span[contains(.,'Biggest Hits')]//ancestor::div[6]//div[@id='contents']//ytd-compact-station-renderer//p[@id='video-count-text']");
+        String res = Wrappers.findElementAndPrint(driver, locator_TrackCount, driver.findElements(locator_TrackCount).size()-1);
+        Thread.sleep((new java.util.Random().nextInt(3) + 2) * 1000);
+        SoftAssert sa = new SoftAssert();
+        sa.assertTrue(Wrappers.convertToNumericValue(res.split(" ")[0])>50);
+    }
+
+    @Test(enabled = true)
+    public void testCase04() throws InterruptedException {
+        System.out.println("Running Test Case 04");
+        Wrappers.findElementAndClick(driver, By.xpath("//a[@title='News']"));
+        Thread.sleep((new java.util.Random().nextInt(3) + 2) * 1000);
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    
+        // Wait for the element to be clickable
+        WebElement contentCards = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@id='rich-shelf-header-container' and contains(.,'Latest news posts')]//ancestor::div[1]//div[@id='contents']")));
+    
+        Thread.sleep((new java.util.Random().nextInt(3) + 2) * 1000);
+        long sumOfVotes = 0;
+        for (int i = 1; i <= 3; i++) {
+            System.out.println(Wrappers.findElementAndPrintWE(driver, By.xpath("//div[@id='header']"), contentCards, i));
+            System.out.println(Wrappers.findElementAndPrintWE(driver, By.xpath("//div[@id='body']"), contentCards, i));
+            try {
+                String res = Wrappers.findElementAndPrintWE(driver, By.xpath("//span[@id='vote-count-middle']"), contentCards, i);
+                if (!res.isEmpty()) { // Check if res is not empty
+                    sumOfVotes += Wrappers.convertToNumericValue(res);
+                }
+            } catch (NoSuchElementException e) {
+                System.out.println("Vote not present - " + e.getMessage());
+            } catch (IllegalArgumentException e) {
+                System.out.println("Error converting to numeric value - " + e.getMessage());
+            }
+            System.out.println(sumOfVotes);
+            Thread.sleep((new java.util.Random().nextInt(3) + 2) * 1000);
+        }
+        System.out.println("Ending Test Case 04");
+    }
+    @Test(enabled = true, dataProvider = "excelData")
+    public void testCase05(String searchWord) throws InterruptedException {
+        System.out.println("Running Test Case 05 Flow for: " + searchWord);
+        Wrappers.sendKeysWrapper(driver, By.xpath("//input[@id='search']"), searchWord);
+        Thread.sleep((new java.util.Random().nextInt(3) + 2) * 1000);
+        long tally = 0;
+        int iter = 1;
+        while(tally<100000000 || iter > 5){
+            String res =  Wrappers.findElementAndPrint(driver, By.xpath("(//div[@class='style-scope ytd-video-renderer' and @id='meta']//span[@class='inline-metadata-item style-scope ytd-video-meta-block'][1])"),iter);
+            res = res.split(" ")[0];
+            tally += Wrappers.convertToNumericValue(res);
+            Thread.sleep((new java.util.Random().nextInt(3) + 2) * 1000);
+        }
+        Thread.sleep((new java.util.Random().nextInt(3) + 2) * 1000);
+        System.out.println("Ending Test Case 05");
+    }
+
 
         @AfterTest
         public void endTest() {
--- src/test/java/demo/wrappers/Wrappers.java
+++ src/test/java/demo/wrappers/Wrappers.java
@@ -14,4 +14,120 @@ public class Wrappers {
     /*
      * Write your selenium wrappers here
      */
+    public static void goToUrlAndWait(WebDriver driver, String url) {
+        driver.get(url);
+        new WebDriverWait(driver, Duration.ofSeconds(10)).until(
+            webDriver -> ((JavascriptExecutor) webDriver).executeScript("return document.readyState").equals("complete")
+        );
+    }
+
+    public static void sendKeysWrapper(WebDriver driver, By locator, String textToSend) {
+        System.out.println("Sending Keys");
+        try {
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+            wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+            WebElement textInput = driver.findElement(locator);
+            textInput.clear();
+            textInput.sendKeys(textToSend);
+            textInput.sendKeys(Keys.ENTER);
+        } catch (Exception e) {
+            System.out.println("Exception Occured! " + e.getMessage());
+        }
+    }
+
+    public static void clickTillUnclickable(WebDriver driver, By locator, Integer maxIterations) throws InterruptedException{
+        Integer numIter = 0;
+        while(numIter<maxIterations){
+            try{
+                findElementAndClick(driver, locator);
+            }
+            catch(TimeoutException e){
+                System.out.println("Stopping - "+e.getMessage());
+                break;
+            }
+        }
+    }
+    
+    
+    public static void findElementAndClick(WebDriver driver, By locator) throws InterruptedException {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+        
+        // Wait for the element to be clickable
+        WebElement element = wait.until(ExpectedConditions.elementToBeClickable(locator));
+        
+        // Wait until element is visible after scrolling
+        wait.until(ExpectedConditions.visibilityOf(element));
+        
+        // Click the element
+        element.click();
+        Thread.sleep(1000);
+    }
+    public static void findElementAndClickWE(WebDriver driver, WebElement we, By locator) {
+        WebElement element = we.findElement(locator);
+        // Scroll to the element
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", element);
+        
+        
+        // Click the element
+        element.click();
+    }
+
+    public static String findElementAndPrint(WebDriver driver, By locator, int elementNo){
+
+        WebElement we = driver.findElements(locator).get(elementNo);
+        // Return the result
+        String txt = we.getText();
+        return txt;
+    }
+
+    public static String findElementAndPrintWE(WebDriver driver, By locator, WebElement we, int elementNo){
+        WebElement element = we.findElements(locator).get(elementNo);
+
+        String txt = element.getText();
+        return txt;
+    }
+
+    public static long convertToNumericValue(String value) {
+        // Trim the string to remove any leading or trailing spaces
+         // Logging input value and length for debugging
+    System.out.println("Input value: " + value);
+    System.out.println("Length of input value: " + value.length());
+
+    // Trim the string to remove any leading or trailing spaces
+    value = value.trim().toUpperCase();
+
+    // Check if the string is empty after trimming
+    if (value.isEmpty()) {
+        throw new IllegalArgumentException("Empty string provided");
+    }
+
+    // Check if the last character is non-numeric and determine the multiplier
+    char lastChar = value.charAt(value.length() - 1);
+    int multiplier = 1;
+    switch (lastChar) {
+        case 'K':
+            multiplier = 1000;
+            break;
+        case 'M':
+            multiplier = 1000000;
+            break;
+        case 'B':
+            multiplier = 1000000000;
+            break;
+        default:
+            // If the last character is numeric, parse the entire string
+            if (Character.isDigit(lastChar)) {
+                return Long.parseLong(value);
+            }
+            throw new IllegalArgumentException("Invalid format: " + value);
+    }
+
+    // Extract the numeric part before the last character
+    String numericPart = value.substring(0, value.length() - 1);
+    double number = Double.parseDouble(numericPart);
+
+    // Calculate the final value
+    return (long) (number * multiplier);
+}
+
 }
